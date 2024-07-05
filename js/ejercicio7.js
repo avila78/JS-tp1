eaaa5f40f430a628332993a61f5eef8559bf94e0
Luis Avila <avilaluisfernando@hotmail.com>
2024-07-05 12:07:26 -0300

    se escribio el ejercicio 8 con condicionales

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -7,10 +7,8 @@ const numero3 = parseInt(
 );
 if (numero1 > numero2 && numero1 > numero3) {
   document.write(`El ${numero1} es el mas grande`);
+} else if (numero2 > numero1 && numero2 > numero3) {
+  document.write(`El ${numero2} es el mas grande`);
 } else {
-  if (numero2 > numero1 && numero2 > numero3) {
-    document.write(`El ${numero2} es el mas grande`);
-  } else {
-    document.write(`El ${numero3} es el mas grande`);
-  }
+  document.write(`El ${numero3} es el mas grande`);
 }
